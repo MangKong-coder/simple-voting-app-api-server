b211784feb7181f15cb48b669cf7e206a579c32c
James Domingo <domango1214@gmail.com>
2021-11-25 14:13:20 +0800

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+const App = () => {
+    return (
+        <div>
+            <Container>
+            <h1>Hello World</h1>
+            </Container>
+        </div>
+    )
+}
+
+export default App
